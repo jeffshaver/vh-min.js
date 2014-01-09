0ba950423249723335e40f44358f47d1badbdc61
jeffshaver <jeff.e.shaver@gmail.com>
2014-01-08 22:53:06 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -2,3 +2,17 @@ vh-min.js
 =========
 
 JS that can be used to simulate vh unit functionality (since it isn't supported well on mobile)
+
+When I was making something for my own site, there was a particular section that I wanted to be the full height of the viewport. There are vh and vw units that do exactly this. However, because of poor support (especially on mobile devices... iOS, Android, etc...) I needed a more effective way of doing this. Enter vh-min.js
+
+Usage
+=====
+
+    <div id="element-that-needs-viewport-height" data-vhmin="true"></div>
+
+The data-vhmin="true" attribute is what indicates this element should be at least a full viewport.
+
+The only other option that is allowed is data-vhmin-offset="number". Adding this to your element will subtract pixels from the elements height
+
+    // Will remove 100 px from the viewport height
+    <div id="element-that-needs-viewport-height" data-vhmin="true" data-vhmin-offset="100"></div>
---
+++ vh-min.js
@@ -0,0 +1,34 @@
+var vhmin = function() {
+  var getElements = function() {
+    Array.prototype.slice.call(document.querySelectorAll('[data-vhmin]')).forEach(function(item, index, array) {
+      this.elements.push({
+        element: item,
+        offset: parseInt(item.getAttribute('data-vhmin-offset'), 10)
+      });
+    }, this);
+  };
+  
+  var calculateHeight = function() {
+    var windowHeight = window.innerHeight;
+    this.elements.forEach(function(item, index, array) {
+      var childrenHeight = 0;
+      Array.prototype.slice.call(item.element.children).forEach(function(item, index, array) {
+        childrenHeight += parseInt(window.getComputedStyle(item).height);
+      }, this);
+      if (childrenHeight + item.offset < windowHeight) {
+        item.element.style.height = (windowHeight - item.offset) + 'px';
+      } else if (childrenHeight + item.offset > windowHeight) {
+        console.log(childrenHeight + 'px');
+        item.element.style.height = childrenHeight + 'px'
+      }
+    }, this);
+  }
+  return {
+    elements: [],
+    initialize: function() {
+      getElements.call(this);
+      calculateHeight.call(this);
+      window.addEventListener('resize', calculateHeight.bind(this), false);
+    }
+  }
+}();
