5fda16210fe0c42bba119e46379ce8901627189e
PlNG <tempeml@hotmail.com>
2014-01-13 09:35:44 -0500

    requested /r/Javascript code review.
    Code now satisfies JSLint, JSHint, and Closure-Compiler.

--- vh-min.js
+++ vh-min.js
@@ -1,48 +1,52 @@
-var vhmin = function() {
-  var getElements = function() {
-    Array.prototype.slice.call(document.querySelectorAll('[data-vhmin]')).forEach(function(item, index, array) {
-      var vhminOffset = item.getAttribute('data-vhmin-offset'),
-            offset;
+var vhmin = (function () {
+  'use strict';
+  var getElements = function () {
+    Array.prototype.slice.call(document.querySelectorAll('[data-vhmin]')).forEach(function (item) {
+      var offset, vhminOffset = item.getAttribute('data-vhmin-offset');
       if (!isNaN(parseInt(vhminOffset, 10))) {
         offset = parseInt(vhminOffset, 10);
-      } else if (!vhminOffset) {
-        offset = 0;
-      } else if (isNaN(parseInt(vhminOffset, 10))) {
-        offset = document.querySelector(vhminOffset).offsetHeight;
-      } 
+      } else {
+        if (!vhminOffset) {
+          offset = 0;
+        } else {
+          if (isNaN(parseInt(vhminOffset, 10))) {
+            offset = document.querySelector(vhminOffset).offsetHeight;
+          }
+        }
+      }
       this.elements.push({
         element: item,
         offset: offset
       });
     }, this);
-  };
-  
-  var debounce = function(fn, delay) {
-    var timer = null,
-          context = this;
-    return function() {
-      clearTimeout(timer);
-      timer = setTimeout(fn.bind(context), delay);
-    }
-  }
-  
-  var calculateHeight = function() {
-    var windowHeight = window.innerHeight;
-    this.elements.forEach(function(item, index, array) {
-      var childrenHeight = 0;
-      Array.prototype.slice.call(item.element.children).forEach(function(item, index, array) {
-        childrenHeight += item.offsetHeight;
+  },
+    debounce = function (fn, delay) {
+      var timer = null,
+        context = this;
+      return function () {
+        clearTimeout(timer);
+        timer = setTimeout(fn.bind(context), delay);
+      };
+    },
+    calculateHeight = function () {
+      var windowHeight = window.innerHeight;
+      this.elements.forEach(function (item) {
+        var childrenHeight = 0;
+        Array.prototype.slice.call(item.element.children).forEach(function (item) {
+          childrenHeight += item.offsetHeight;
+        }, this);
+        if (childrenHeight + item.offset < windowHeight) {
+          item.element.style.height = (windowHeight - item.offset) + 'px';
+        } else {
+          if (childrenHeight + item.offset > windowHeight) {
+            item.element.style.height = childrenHeight + 'px';
+          }
+        }
       }, this);
-      if (childrenHeight + item.offset < windowHeight) {
-        item.element.style.height = (windowHeight - item.offset) + 'px';
-      } else if (childrenHeight + item.offset > windowHeight) {
-        item.element.style.height = childrenHeight + 'px'
-      }
-    }, this);
-  }
+    };
   return {
     elements: [],
-    init: function(debounceDelay) {
+    init: function (debounceDelay) {
       getElements.call(this);
       calculateHeight.call(this);
       if (debounceDelay !== false) {
@@ -51,5 +55,5 @@ var vhmin = function() {
         window.addEventListener('resize', calculateHeight.bind(this), false);
       }
     }
-  }
-}();
+  };
+}());
